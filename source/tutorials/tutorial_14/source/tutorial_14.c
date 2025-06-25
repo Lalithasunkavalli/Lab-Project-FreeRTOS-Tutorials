63ec45102cbd47ac710b088eca2a624dd9ec9425
Lalitha Sunkavalli <sunkavalli24@gmail.com>
2025-06-24 22:18:41 -0500

    PI and PI

--- source/tutorials/tutorial_14/source/tutorial_14.c
+++ source/tutorials/tutorial_14/source/tutorial_14.c
@@ -1,115 +1,61 @@
-/*
- * FreeRTOS-Tutorials
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * SPDX-License-Identifier: MIT-0
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
- *
- */
-
-/* Standard includes. */
-#include <stdio.h>
-#include <unistd.h>
-
-/* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
+#include "stdio.h"
 
-/**
- * @brief Functions that implement FreeRTOS tasks.
- */
-static void prvHighPriorityTask( void * pvParams );
-static void prvLowPriorityTask( void * pvParams );
-/*-----------------------------------------------------------*/
+#define HIGH_PRIORITY   ( tskIDLE_PRIORITY + 3 )
+#define MEDIUM_PRIORITY ( tskIDLE_PRIORITY + 2 )
+#define LOW_PRIORITY    ( tskIDLE_PRIORITY + 1 )
 
-/**
- * @brief Mutex used in this program.
- */
-static SemaphoreHandle_t xMutex = NULL;
-/*-----------------------------------------------------------*/
+static SemaphoreHandle_t xMutex;
 
-/**
- * @brief Tutorial entry point.
- */
+static void vTaskLow( void *pvParameters )
+{
+    printf("Low: My priority is %d.\r\n", uxTaskPriorityGet( NULL ));
+    vTaskDelay( pdMS_TO_TICKS( 10 ) );
+    printf("Low: Trying to TAKE mutex...\r\n");
+    xSemaphoreTake( xMutex, portMAX_DELAY );
+    printf("Low: got it. Doing work for 250ms...\r\n");
+    vTaskDelay( pdMS_TO_TICKS( 250 ) );
+    printf("Low: My priority is %d.\r\n", uxTaskPriorityGet( NULL )); 
+    printf("Low: RELEASING mutex.\r\n");
+    // print priority of this task
+    xSemaphoreGive( xMutex );
+    vTaskDelete( NULL );
+}
+
+
+static void vTaskMedium( void *pvParameters )
+{
+    int count = 0;
+    vTaskDelay( pdMS_TO_TICKS( 25 ) );
+    for( ;; )
+    {
+        printf("Medium: Running (but Low may inherit my priority)... %d and pritority %d \r\n", count++ ,uxTaskPriorityGet( NULL ));
+        vTaskDelay( pdMS_TO_TICKS( 50 ) );
+    }
+}
+
+static void vTaskHigh( void *pvParameters )
+{
+    vTaskDelay( pdMS_TO_TICKS( 20 ) );
+    printf("High: Trying to TAKE mutex...\r\n");
+    printf("High: My priority is %d.\r\n", uxTaskPriorityGet( NULL ));
+    xSemaphoreTake( xMutex, portMAX_DELAY );
+    printf("High: got it! Immediately releasing.\r\n");
+    xSemaphoreGive( xMutex );
+    vTaskDelete( NULL );
+}
+
 int main( void )
 {
-    BaseType_t xTaskCreationResult = pdFAIL;
-
-    xTaskCreationResult = xTaskCreate( prvHighPriorityTask,
-                                       "High",
-                                       configMINIMAL_STACK_SIZE,
-                                       NULL,
-                                       tskIDLE_PRIORITY + 1,
-                                       NULL );
-    configASSERT( xTaskCreationResult == pdPASS );
-
-    xTaskCreationResult = xTaskCreate( prvLowPriorityTask,
-                                       "Low",
-                                       configMINIMAL_STACK_SIZE,
-                                       NULL,
-                                       tskIDLE_PRIORITY,
-                                       NULL );
-    configASSERT( xTaskCreationResult == pdPASS );
-
+    /* Create a mutex (with priority inheritance enabled in FreeRTOSConfig.h) */
     xMutex = xSemaphoreCreateMutex();
-    configASSERT( xMutex != NULL );
 
-    /* Start the scheduler. */
+    xTaskCreate( vTaskLow,    "Low",    configMINIMAL_STACK_SIZE, NULL, LOW_PRIORITY,    NULL );
+    xTaskCreate( vTaskMedium, "Medium", configMINIMAL_STACK_SIZE, NULL, MEDIUM_PRIORITY, NULL );
+    xTaskCreate( vTaskHigh,   "High",   configMINIMAL_STACK_SIZE, NULL, HIGH_PRIORITY,   NULL );
+
     vTaskStartScheduler();
-
-    /* Should not reach here. */
-    for( ;; )
-    {
-
-    }
-
-    /* Just to make the compiler happy. */
-    return 0;
+    for( ;; );
 }
-/*-----------------------------------------------------------*/
-
-static void prvHighPriorityTask( void * pvParams )
-{
-    /* Silence warning about unused parameters. */
-    ( void ) pvParams;
-
-    for( ;; )
-    {
-        vTaskDelay( pdMS_TO_TICKS( 1000 ) );
-
-        xSemaphoreTake( xMutex, portMAX_DELAY );
-    }
-}
-/*-----------------------------------------------------------*/
-
-static void prvLowPriorityTask( void * pvParams )
-{
-    /* Silence warning about unused parameters. */
-    ( void ) pvParams;
-
-    xSemaphoreTake( xMutex, portMAX_DELAY );
-
-    for( ;; )
-    {
-        fprintf( stderr, "Task priority is %lu\r\n", uxTaskPriorityGet( NULL ) );
-
-        vTaskDelay( pdMS_TO_TICKS( 1000 ) );
-    }
-}
-/*-----------------------------------------------------------*/
